d7e1fa0c955193dba1b5b5cd126a9abed1ddc2dd
bryanchae <bryan450497129@gmail.com>
2014-09-27 10:24:42 +0800

    bryan's homework

--- app/src/main/java/tw/soleil/androidvillage/activity/java/homework/ControllingExecutionActivity.java
+++ app/src/main/java/tw/soleil/androidvillage/activity/java/homework/ControllingExecutionActivity.java
@@ -12,6 +12,10 @@ package tw.soleil.androidvillage.activity.java.homework;
 
 import android.os.Bundle;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import tw.soleil.androidvillage.R;
 import tw.soleil.androidvillage.activity.ChapterActivity;
 
@@ -39,43 +43,98 @@ public class ControllingExecutionActivity extends ChapterActivity {
         // Learning if-else
         displayResultText.append("Learning if-else");
         displayResultText.append("\n");
-        // TODO start here
+        int temperature = 33;
+        if (temperature >= 30) {
+            displayResultText.append("hot weather with temperature " + temperature + " celsius");
+        }
         displayResultText.append("\n\n");
 
         // Learning true and false
         displayResultText.append("Learning true and false");
         displayResultText.append("\n");
-        // TODO start here
+            displayResultText.append("\n");
+            boolean isBigger;
+            isBigger = 5 > 3;
+            displayResultText.append("5 > 3 is " + String.valueOf(isBigger));
         displayResultText.append("\n\n");
 
         // Learning Iteration
         displayResultText.append("Learning Iteration");
         displayResultText.append("\n");
-        // TODO start here
+            String[] colors = new String[]{"Red", "Yellow", "Blue", "Green","gray"};
+            for (int index = 0; index < colors.length; index ++) {
+                String eachColor = colors[index];
+                displayResultText.append(eachColor);
+            }
         displayResultText.append("\n\n");
 
         // Learning for each
         displayResultText.append("Learning for each");
         displayResultText.append("\n");
-        // TODO start here
+         for (String color : colors) {
+           displayResultText.append(color);
+           displayResultText.append(" ");
+                }
         displayResultText.append("\n\n");
 
         // Learning iterator
         displayResultText.append("Learning Iterator");
         displayResultText.append("\n");
-        // TODO start here
-        displayResultText.append("\n\n");
+                ArrayList<String> colorList = new ArrayList<String>();
+                colorList.add("Red");
+                colorList.add("white");
+                colorList.add("Yellow");
+                colorList.add("Blue");
+                colorList.add("Green");
+            for (Iterator<String> eachColor = colorList.iterator(); eachColor.hasNext();)
+            {
+                String eachColorText = eachColor.next();
+                displayResultText.append(eachColorText);
+            }
+            displayResultText.append("\n\n");
 
         // Learning break
         displayResultText.append("Learning break");
         displayResultText.append("\n");
-        // TODO start here
+                for (String colortext : colorList) {
+                    displayResultText.append(colortext);
+                    displayResultText.append(" ");
+                    if (colortext.equalsIgnoreCase("pink" )) {
+                        displayResultText.append("and we calling break...");
+                        break;
+                    }
+                }
         displayResultText.append("\n\n");
 
         // Learning switch
         displayResultText.append("Learning switch");
         displayResultText.append("\n");
-        // TODO start here
+                int johnId = 30;
+                int annId = 2;
+                switch (johnId) {
+                    case 3:
+                        displayResultText.append("John's id is 3.");
+                        displayResultText.append("\n");
+                        break;
+                    case 2:
+                        displayResultText.append("John's id is 2.");
+                        displayResultText.append("\n");
+                        break;
+                    default:
+                        displayResultText.append("John's id no matching.");
+                        displayResultText.append("\n");
+                        break;
+                }
+                switch (annId) {
+                    case 1:
+                        displayResultText.append("Ann's id is 1.");
+                        displayResultText.append("\n");
+                        break;
+                    case 2:
+                        displayResultText.append("Ann's id is 2.");
+                        displayResultText.append("\n");
+                        break;
+            }
         displayResultText.append("\n\n");
 
         fullScreenTextView.setText(displayResultText.toString());
