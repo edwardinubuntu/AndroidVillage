7870ff4f825ce185a2b828248e097c1c12166236
bryanchae <bryan450497129@gmail.com>
2014-10-18 12:46:19 +0800

    fragment calculator

--- app/src/main/java/tw/soleil/androidvillage/Fragment/CalculatorFragment.java
+++ app/src/main/java/tw/soleil/androidvillage/Fragment/CalculatorFragment.java
@@ -1,7 +1,28 @@
 package tw.soleil.androidvillage.Fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import tw.soleil.androidvillage.R;
+
 /**
  * Created by bryan on 2014/10/18.
  */
-public class CalculatorFragment extends PlaceholderFragment{
+public class CalculatorFragment extends PlaceholderFragment{public static CalculatorFragment newInstance(int sectionNumber) {
+    CalculatorFragment fragment = new CalculatorFragment();
+    Bundle args = new Bundle();
+    args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+    fragment.setArguments(args);
+    return fragment;
+     }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_calculator, container, false);
+
+        return  rootView;
+    }
 }
