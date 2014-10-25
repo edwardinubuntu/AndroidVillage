6ea49857c6a3050727551dd2db32a159effaa3db
bryanchae <bryan450497129@gmail.com>
2014-10-25 11:36:59 +0800

    bryan

--- app/src/main/java/tw/soleil/androidvillage/Fragment/CalculatorFragment.java
+++ app/src/main/java/tw/soleil/androidvillage/Fragment/CalculatorFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import tw.soleil.androidvillage.R;
 
@@ -22,6 +23,24 @@ public class CalculatorFragment extends PlaceholderFragment{public static Calcul
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_calculator, container, false);
+
+        Button button_1 = (Button) rootView.findViewById(R.id.button_one);
+
+        Button button_2 = (Button) rootView.findViewById(R.id.button_two);
+
+        Button button_3 = (Button) rootView.findViewById(R.id.button_three);
+
+        Button button_4 = (Button) rootView.findViewById(R.id.button_four);
+
+        Button button_5 = (Button) rootView.findViewById(R.id.button_five);
+
+        Button button_6 = (Button) rootView.findViewById(R.id.button_six);
+
+        Button button_7 = (Button) rootView.findViewById(R.id.button_seven);
+
+        Button button_8 = (Button) rootView.findViewById(R.id.button_eight);
+
+        Button button_9 = (Button) rootView.findViewById(R.id.button_nine);
 
         return  rootView;
     }
