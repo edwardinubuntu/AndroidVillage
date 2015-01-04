983df08ff2c498198af7819882c6193ebf6f6372
bryanchae <bryan450497129@gmail.com>
2015-01-04 23:36:41 +0800

    google map

--- app/src/main/java/tw/soleil/androidvillage/Fragment/MyGoogleMapFragment.java
+++ app/src/main/java/tw/soleil/androidvillage/Fragment/MyGoogleMapFragment.java
@@ -10,17 +10,21 @@
 
 package tw.soleil.androidvillage.Fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import tw.soleil.androidvillage.R;
@@ -29,6 +33,10 @@ import tw.soleil.androidvillage.R;
  * Created by edward_chiang on 15/1/3.
  */
 public class MyGoogleMapFragment extends PlaceholderFragment implements OnMapReadyCallback {
+
+    private MarkerOptions myHomeMarker;
+
+    private WebView webView;
 
     public static MyGoogleMapFragment newInstance(int sectionNumber) {
         MyGoogleMapFragment fragment = new MyGoogleMapFragment();
@@ -42,22 +50,94 @@ public class MyGoogleMapFragment extends PlaceholderFragment implements OnMapRea
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_my_google_map, container, false);
 
-        MapFragment mapFragment = (MapFragment)getFragmentManager().findFragmentById(R.id.my_google_map);
+        MapFragment mapFragment = (MapFragment)getFragmentManager().findFragmentById(R.id.my_google_map_view);
         mapFragment.getMapAsync(this);
+
+        webView = (WebView)rootView.findViewById(R.id.my_google_map_web_view);
+        webView.setVisibility(View.INVISIBLE);
 
         return rootView;
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        LatLng taipei101 = new LatLng(25.21, 121.3354);
+        LatLng taipei101 = new LatLng(25.0336, 121.5650);
 
         googleMap.setMyLocationEnabled(true);
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(taipei101, 13));
 
         googleMap.addMarker(new MarkerOptions()
                 .title("taipei101")
                 .snippet("the highest building in Taiwan")
                 .position(taipei101));
+
+        LatLng homeSweetHome = new LatLng(25.027509,121.538452);
+
+        myHomeMarker = new MarkerOptions()
+                .title("琉璃藏")
+                .snippet("琉璃藏")
+                .position(homeSweetHome);
+
+        googleMap.addMarker(myHomeMarker );
+
+
+
+
+
+
+        LatLng home = new LatLng(24.784539,121.017765);
+
+        myHomeMarker = new MarkerOptions()
+                .title("北歐")
+                .snippet("北歐")
+                .position(home);
+
+        googleMap.addMarker(myHomeMarker);
+
+
+
+
+        LatLng slash = new LatLng(25.034731,121.521934);
+
+        myHomeMarker = new MarkerOptions()
+                .title("中正紀念堂")
+                .snippet("中正紀念堂")
+                .position(slash);
+
+        googleMap.addMarker(myHomeMarker);
+
+
+
+
+        LatLng air1 = new LatLng(25.079651,121.234217);
+
+        myHomeMarker = new MarkerOptions()
+                .title("臺灣桃園國際機場")
+                .snippet("臺灣桃園國際機場")
+                .position(air1);
+
+        googleMap.addMarker(myHomeMarker);
+        // Home 2
+
+        // 中正紀念堂
+
+        googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+
+                marker.showInfoWindow();
+
+
+                String url = "http://maps.googleapis.com/maps/api/streetview?size=200x200&location="+marker.getPosition().latitude+",%20"+marker.getPosition().longitude+"&fov=90&heading=235&pitch=10&sensor=false";
+                webView.loadUrl(url);
+
+                webView.setVisibility(View.VISIBLE);
+
+                return true;
+            }
+        });
+
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(taipei101, 13));
+
     }
 }
