d5919a643361d9ff5f3c15c1f5eeca42992f8cf1
bryanchae <bryan450497129@gmail.com>
2015-03-07 11:53:49 +0800

    vvvvvv

--- stickwar/src/main/java/com/example/bryan/stickwar/MainActivity.java
+++ stickwar/src/main/java/com/example/bryan/stickwar/MainActivity.java
@@ -1,19 +1,80 @@
 package com.example.bryan.stickwar;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.transition.Slide;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.parse.FindCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends ActionBarActivity {
+
+
+    private ViewPager viewPager;
+    private PagerAdapter pagerAdapter;
+
+    private List<ParseObject> stickManWarList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        stickManWarList = new ArrayList<>();
+
+        Parse.initialize(this, "YRaYVYv0C1UykLKYUbuaPv53LS9VSNnp6SWzqUyh", "PY9InNYVa5YR2z7RfdnMtW8BRl4R4GJBSHntdKwC");
     }
 
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        viewPager = (ViewPager) findViewById(R.id.cardViewPager);
+        pagerAdapter = new SlidePagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+
+        loadStickManWar();
+    }
+
+    private void loadStickManWar() {
+
+
+        ParseQuery stickmanQuery = new ParseQuery("StickMan");
+        stickmanQuery.findInBackground(new FindCallback() {
+            @Override
+            public void done(List list, ParseException e) {
+                if (e !=null) {
+                    Log.e("StickMan", "Parse find in background: " + e.getLocalizedMessage());
+                }
+
+                if(list!= null){
+                    stickManWarList.clear();
+                    stickManWarList.addAll(list);
+                    pagerAdapter.notifyDataSetChanged();
+
+
+                }
+            }
+        });
+
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,4 +97,24 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private  class SlidePagerAdapter extends FragmentStatePagerAdapter {
+
+
+        public SlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return new StickManFragment(stickManWarList.get(position));
+        }
+
+        @Override
+        public int getCount() {
+            return stickManWarList.size();
+        }
+    }
+
+
 }
---
+++ stickwar/src/main/java/com/example/bryan/stickwar/StickManFragment.java
@@ -0,0 +1,48 @@
+package com.example.bryan.stickwar;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.koushikdutta.ion.Ion;
+import com.parse.ParseObject;
+
+/**
+ * Created by bryan on 2015/3/7.
+ */
+public class StickManFragment extends Fragment {
+
+    private ParseObject dataObject;
+
+    public StickManFragment() {
+    }
+
+    public StickManFragment(ParseObject dataObject) {
+        this.dataObject = dataObject;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_stick_man_war,container,false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        TextView titleTextView = (TextView) view.findViewById(R.id.title_text_view);
+        titleTextView.setText(dataObject.getString("title"));
+
+        ImageView imageView = (ImageView)view.findViewById(R.id.stick_man_image_view);
+
+        Ion.with(getActivity())
+                .load(dataObject.getParseFile("pictureURL").getUrl())
+                .intoImageView(imageView);
+    }
+}
